7b891b0601f656f27aae3a11fab6bbce05761981
Daniel <daniel.k.berglund@gmail.com>
2014-05-12 14:28:01 +0200

    Added new enum Recode, moved enums to data/enums

---
+++ src/data/enums/Recode.h
@@ -0,0 +1,18 @@
+#ifndef RECODE_H_
+#define RECODE_H_
+
+/**
+ * This is ...
+ *
+ * @author Daniel Berglund daniel.k.berglund@gmail.com
+ */
+
+namespace CuEira {
+
+enum Recode {
+  ALL_RISK, SNP_PROTECT, ENVIRONMENT_PROTECT, INTERACTION_PROTECT
+};
+
+} /* namespace CuEira */
+
+#endif /* RECODE_H_ */

59624e8d81847c7c183a89865e75569032087c42
Daniel <daniel.k.berglund@gmail.com>
2014-09-22 17:50:15 +0200

    Fixed printing of header

--- src/fileIO/writers/ResultWriter.cpp
+++ src/fileIO/writers/ResultWriter.cpp
@@ -32,8 +32,11 @@ void ResultWriter::writePartialResult(const Model::ModelInformation* modelInform
 }
 
 void ResultWriter::printHeader() {
-  outputStream
-      << "snp_id,pos,skip,risk_allele,minor,major,env_id,ap,reri,OR_snp,OR_snp_L,OR_snp_H,OR_env,OR_env_L,OR_env_H,OR_inter,OR_inter_L,OR_inter_H,";
+  outputStream << "snp_id,pos,skip,risk_allele,minor,major,env_id,"
+      << "no_alleles_minor_case,no_alleles_major_case,no_alleles_minor_control,no_alleles_major_control,no_alleles_minor,no_alleles_major,"
+      << "freq_alleles_minor_case,freq_alleles_major_case,freq_alleles_minor_control,freq_alleles_major_control,freq_alleles_minor,freq_alleles_major,"
+      << "no_snp0_env0_case,no_snp0_env0_control,no_snp1_env0_case,no_snp1_env0_control,no_snp0_env1_case,no_snp0_env1_control,no_snp1_env1_case,no_snp1_env1_control,"
+      << "ap,reri,OR_snp,OR_snp_L,OR_snp_H,OR_env,OR_env_L,OR_env_H,OR_inter,OR_inter_L,OR_inter_H,";
 
   /*
    for(int i = 0; i < numberOfCovariates; ++i){
